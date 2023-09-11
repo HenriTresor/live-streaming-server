c05bdd0d898c9fcc01c9e624a993209e6c6cfd05
henritresor <shimwamanahenritresor@gmail.com>
2023-09-11 16:33:03 +0200

    initialised mongoose connection

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 import express from "express";
 import { Server } from "socket.io";
 import http from "http";
+import dbConfig from "./configs/db.config.js";
 const app = express();
+const port = process.env.PORT || 8080;
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
@@ -9,5 +11,8 @@ const io = new Server(server, {
         credentials: true,
     },
 });
-
-
+dbConfig().then(() => {
+    server.listen(port || 8080, () => {
+        console.log(`server is fire on ${port}`);
+    });
+});
--- app.ts
+++ app.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response, Application } from "express";
 import { Server } from "socket.io";
 import http from "http";
+import dbConfig from "./configs/db.config.js";
 
 const app: Application = express();
+const port = process.env.PORT || 8080;
 const server = http.createServer(app);
 
 const io = new Server(server, {
@@ -11,3 +13,10 @@ const io = new Server(server, {
     credentials: true,
   },
 });
+
+dbConfig().then(() => {
+  server.listen(port || 8080, () => {
+    console.log(`server is fire on ${port}`);
+  });
+});
+
