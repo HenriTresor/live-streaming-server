27b3b14ee7aa984e212677c5af58e40f9d0afd97
henritresor <shimwamanahenritresor@gmail.com>
2023-09-11 18:08:44 +0200

    modified model, added fields for handling friends

--- models/User.model.ts
+++ models/User.model.ts
@@ -5,6 +5,11 @@ interface User {
   email: string;
   fullName: string;
   password: string;
+  friends: User[];
+  sentRequests: User[];
+  friendRequests: User[];
+  followers: User[];
+  following: User[];
 }
 
 const UserSchema = new mongoose.Schema<User>(
@@ -12,6 +17,37 @@ const UserSchema = new mongoose.Schema<User>(
     email: { type: String, required: true, unique: true, trim: true },
     fullName: { type: String, required: true, trim: true },
     password: { type: String, required: true, trim: true },
+    friendRequests: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+      },
+    ],
+    friends: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+      },
+    ],
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+      },
+    ],
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+      },
+    ],
+
+    sentRequests: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+      },
+    ],
   },
 
   {
