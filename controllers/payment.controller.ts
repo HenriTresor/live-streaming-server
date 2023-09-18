3c2201302d743f201d1cc8bee925a2b491e6c65d
henritresor <shimwamanahenritresor@gmail.com>
2023-09-18 15:06:15 +0200

    finished buying coins

--- controllers/payment.controller.ts
+++ controllers/payment.controller.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import stripe from "../configs/stripe.config.js";
 import errorResponse from "../utils/errorResponse.js";
+import { getCoins } from "../services/payment.js";
 
 export const createCheckoutSession = async (
   req: Request,
@@ -8,20 +9,23 @@ export const createCheckoutSession = async (
   next: NextFunction
 ) => {
   try {
-    console.log(req.body);
+    const coins = await getCoins();
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["paypal", "card"],
       mode: "payment",
       line_items: req.body.items?.map((item: any) => {
+        const coinItem = coins?.find(
+          (coin) => Number(coin.price) === Number(item.price)
+        );
         return {
           price_data: {
             currency: "usd",
-            unit_amount: 1000,
             product_data: {
-              name: item.name,
+              name: coinItem?.amount,
             },
+            unit_amount: coinItem?.price,
           },
-          quantity: item.quantity,
+          quantity: item.amount,
         };
       }),
       success_url: `${process.env.SERVER_URL}/api/v1/stripe/success`,
